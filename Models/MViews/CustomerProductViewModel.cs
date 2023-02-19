4c90bf9d1c8ab3d3ad834f74584bb7226eecce12
abdullah-aladham <abdullah.aladham@gmail.com>
2023-02-19 18:06:27 +0200

    Fixed some errors in Task#2 in 57:09 mins feb 16,2023

--- Models/MViews/CustomerProductViewModel.cs
+++ Models/MViews/CustomerProductViewModel.cs
@@ -8,5 +8,15 @@
         public int ProductId { get; set; }
         
          public virtual Products Product { get; set; }
+        public CustomerProductViewModel(int id, int customerId, int productId)
+        {
+            Id = id;
+            CustomerId = customerId;
+            ProductId = productId;
+        }
+        public CustomerProductViewModel()
+        {
+
+        }
     }
 }
